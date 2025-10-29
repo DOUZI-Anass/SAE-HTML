07de6f471c4cf6e0922b6f219b8cc2b340dc32b6
RedaBouk <148530067+RedaBouk@users.noreply.github.com>
2025-10-29 12:26:38 +0100

    Add files via upload

---
+++ vivreEnBonneSante.php
@@ -0,0 +1,110 @@
+<?php
+$page = 'autre';
+require 'header.php';
+?>
+<body class="qsn-page">
+
+<section class="container py-5 mt-5">
+    <h2 class="section-title text-center mb-4">Vivre en bonne Santé</h2>
+    <p class="lead text-center">
+        Il est aujourd’hui avéré que les questions de santé sont directement liées à la question du bien-être et à sa promotion. C’est la raison pour laquelle la FAGE mène des
+        actions ayant pour objectif d’intervenir auprès des étudiants afin de prévenir les situations de mal-être et la vulnérabilité. Ces programmes ont pour objectif de susciter une prise de conscience
+        des étudiants sur leur situation, leurs habitudes et comportements, parfois à risque, et de donner des moyens pour pouvoir y remédier.
+    </p>
+</section>
+
+
+
+<!-- SECTION CONTENU -->
+<section class="container py-5 mt-5">
+
+    <div class="row justify-content-center centrale">
+        <div class="col-12 col-md-8 text-center">
+            <h2 class="fw-bold text-uppercase text-primary mb-3">Nos Campagnes</h2>
+        </div>
+    </div>
+
+    <!-- Bloc 1 -->
+    <div class="row align-items-center mb-5">
+        <div class="col-md-6 mb-md-0">
+            <h2 class="fw-bold text-uppercase text-primary">Bouge ton Blues</h2>
+            <p>
+                Partant du constat que le suicide est la deuxième cause de mortalité entre 15 et 24 ans (16 %), la <strong>FAGE</strong> a créé une campagne pour lutter contre le mal-être étudiant. L'action se déploie en deux volets :
+                <ul>
+                    <li>un espace de dépistage et d'orientation dans lequel les étudiants sont invités à compléter des échelles sur des ordinateurs,</li>
+                    <li>un isoloir pour échanger avec un des psychologues autour des résultats et des éventuelles difficultés rencontrées</li>
+                </ul>
+            </p>
+            <p>
+                Une conférence/débat en présence d'intervenants spécialisés sur les questions de suicide peut également compléter cette action.
+            </p>
+        </div>
+        <div class="col-md-6 text-center">
+            <img src="assets/image/AfficheBouge-ton-Blues.png" alt="Affiche de Campagnes" class="imgM">
+        </div>
+    </div>
+
+    <!-- Bloc 2 -->
+    <div class="row align-items-center flex-md-row-reverse mb-5">
+        <div class="col-md-6 mb-md-0">
+            <h2 class="fw-bold text-uppercase text-primary">Bien dans ton assiette !</h2>
+            <p>
+                Souvent confrontés à un changement de mode de vie dès l'entrée à l'université, les étudiants prennent, parfois sans s'en rentre compte,
+                de mauvaises habitudes dans leur vie quotidienne. C'est dans l'objectif de promouvoir les bonnes pratiques en matière de bien-être que
+                la FAGE et Macif-Mutualité ont mis en place une campagne de sensibilisation à l'équilibre alimentaire en milieu étudiant.
+            </p>
+            <p>
+                Sept fiches nutritions ont ainsi été créées afin d'informer et donner des conseils pratiques, facilement adaptables à la vie étudiante
+                (horaires décalés, matériel de cuisson limité, budget restreint, etc.).
+            </p>
+        </div>
+        <div class="col-md-6 text-center">
+            <img src="assets/image/FAGE_Assiette.jpg" alt="Bien dans ton assiette" class="imgM test">
+        </div>
+    </div>
+
+    <!-- Bloc 3 -->
+    <div class="row align-items-center mb-5">
+        <div class="col-md-6 mb-md-0">
+            <h2 class="fw-bold text-uppercase text-primary">Actions de lutte contre le sida</h2>
+            <p>
+                Chaque année en France, près de 6 000 personnes découvrent leur séropositivité. 10 % d'entre elles sont
+                des jeunes âgés de 15 à 24 ans.
+            </p>
+            <p>
+                Il s'agit de ne pas dissocier la collecte de la prévention : la collecte de dons qui est nécessaire pour
+                faire avancer la recherche, et la prévention qui reste le meilleur moyen d'agir pour faire reculer le
+                nombre de nouvelles contaminations chez les jeunes.
+            </p>
+        </div>
+        <div class="col-md-6 text-center">
+            <img src="assets/image/FAGE_Lutte_Sida.png" alt="lutte contre le sida" class="imgM">
+        </div>
+    </div>
+
+    <!-- Bloc 4 -->
+    <div class="row align-items-center flex-md-row-reverse mb-5">
+        <div class="col-md-6 mb-md-0">
+            <h2 class="fw-bold text-uppercase text-primary">L'Enfumage : prévention contre le tabac</h2>
+            <p>
+                La lutte contre le tabac est un réel enjeu de santé publique, d'autant plus auprès des jeunes, car les fabricants
+                du tabac ciblent directement cette population. Comme l'indique l'OMS « La consommation de tabac ne ressemble pas
+                à d'autres menaces sur la santé mondiale. Les maladies infectieuses n'emploient pas des sociétés multinationales
+                de relations publiques. Il n'existe pas de groupes de façade pour promouvoir la propagation du choléra. Les moustiques
+                n'ont pas de lobbyistes à leur service. Le tabac est une maladie contagieuse. La contamination passe par la publicité
+                et le parrainage ».
+            </p>
+            <p>
+                Il convient donc pour les associations étudiantes qui animent les campus, écoles ou instituts, d'informer et d'
+                leurs pairs sur le tabac, ses différentes formes de consommation et ses dangers.
+            </p>
+        </div>
+        <div class="col-md-6 text-center">
+            <img src="assets/image/FAGE_Enfumage.png" alt="L'Enfumage" class="imgM test">
+        </div>
+    </div>
+
+</section>
+
+<?php require 'footer.php'; ?>
+</body>
